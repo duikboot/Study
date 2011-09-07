1970772c9d865caad016d5299b099c916b0cbec7
Arjen Dijkstra <arjen.dijkstra@informaat.nl>
2011-09-07 11:56:48 +0200

    added path interface

---
+++ Eindopdracht_java/interfaces/PathInterface.java
@@ -0,0 +1,12 @@
+package interfaces;
+
+/**
+ *
+ * @author arjend
+ */
+public interface PathInterface {
+    /** return the path in which the images are located.
+     * @return path
+     */
+    public String getPath();
+}
