c8e4e27020e080bcadfcbc901244a7476dfdb0f9
Arjen Dijkstra <arjen.dijkstra@informaat.nl>
2011-09-07 12:05:14 +0200

    import interface package and implement the interface

--- Eindopdracht_java/memory/FaceTest.java
+++ Eindopdracht_java/memory/FaceTest.java
@@ -4,7 +4,9 @@
  */
 package memory;
 
-public class FaceTest extends Test{
+import interfaces.PathInterface;
+
+public class FaceTest extends Test implements PathInterface{
     public String path;
     public String getPath() {
         return path;
